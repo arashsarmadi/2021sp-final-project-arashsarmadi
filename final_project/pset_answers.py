7c700c442c64ddd7bb5e1cb8642f4f5e65106751
Arash Sarmadi <arash.sarmadi@gmail.com>
2021-05-02 21:14:05 -0400

    Added final project structure

---
+++ final_project/pset_answers.py
@@ -0,0 +1,9 @@
+from typing import Dict
+from typing import List
+
+from canvasapi.quiz import QuizSubmissionQuestion
+
+
+def get_answers(pset_questions: List[QuizSubmissionQuestion]) -> List[Dict]:
+    """Creates answers for Canvas quiz questions"""
+    pass
